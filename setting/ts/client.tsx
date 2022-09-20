45748f9ae5f42e6eb0c0000cba95493217c17b8f
TheON2 <sniperad@naver.com>
2022-09-20 10:06:13 +0900

    fix many bug

---
+++ setting/ts/client.tsx
@@ -0,0 +1,10 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+
+import App from './layouts/App';
+
+render (<App />,document.querySelector('#app'));
