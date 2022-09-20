d1e83df5650756f0c77510e348f61621c5ee6788
TheON2 <sniperad@naver.com>
2022-09-20 19:03:59 +0900

    Update: page split

--- alecture/layouts/App/index.tsx
+++ alecture/layouts/App/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import LogIn from '@pages/LogIn';
+import SignUp from '@pages/SignUp';
+import {Redirect, Route, Switch} from "react-router";
 
 const App = () => {
-    return <div>좀만 참아달래!</div>;
-
+    return (
+        <Switch>
+            <Redirect exact path="/" to="/login"/>
+            <Route path={"/login"} component={LogIn}/>
+            <Route path={"/login"} component={LogIn}/>
+        </Switch>
+    );
 };
 
 export default App;
---
+++ alecture/pages/SignUp/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SingUp = () => {
+    return (
+        <div>회원가입</div>
+    )
+}
+
+export default SingUp;
