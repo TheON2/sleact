520f8fa1e7516062dec09a744dbdaf5007d5a488
TheON2 <sniperad@naver.com>
2022-09-22 11:10:19 +0900

    Update : typings

--- alecture/layouts/Workspace/index.tsx
+++ alecture/layouts/Workspace/index.tsx
@@ -4,10 +4,11 @@ import fetcher from "@utils/fetcher";
 import axios from 'axios';
 import {Redirect, Route, Switch} from "react-router";
 import {
+    AddButton,
     Channels, Chats,
     Header, LogOutButton, MenuScroll,
     ProfileImg, ProfileModal,
-    RightMenu,
+    RightMenu, WorkspaceButton,
     WorkspaceName,
     Workspaces,
     WorkspaceWrapper
@@ -15,13 +16,14 @@ import {
 import gravatar from 'gravatar';
 import loadable from "@loadable/component";
 import Menu from "@components/Menu";
+import {Link} from 'react-router-dom';
 
-const Channel = loadable(()=> import('@pages/Channel'));
-const DirectMessage = loadable(()=> import('@pages/DirectMessage'));
+const Channel = loadable(() => import('@pages/Channel'));
+const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 
 const Workspace: FC = ({children}) => {
-    const {data, error, revalidate, mutate} = useSWR('http://localhost:3095/api/users', fetcher,{
+    const {data: userData, error, revalidate, mutate} = useSWR('http://localhost:3095/api/users', fetcher, {
         dedupingInterval: 2000, // 2초
     });
     const [showUserMenu, setShowUserMenu] = useState(false);
@@ -31,16 +33,19 @@ const Workspace: FC = ({children}) => {
             withCredentials: true,
         })
             .then((response) => {
-                mutate(false,false);
+                mutate(false, false);
             })
     }, []);
 
     const onClickUserProfile = useCallback(() => {
-        setShowUserMenu((prev)=>!prev);
-        }, []);
+        setShowUserMenu((prev) => !prev);
+    }, []);
 
+    const onClickCreate = useCallback(() => {
 
-    if (!data) {
+    }, []);
+
+    if (!userData) {
         return <Redirect to="/login"/>
     }
 
@@ -49,13 +54,14 @@ const Workspace: FC = ({children}) => {
             <Header>
                 <RightMenu>
                     <span onClick={onClickUserProfile}>
-                        <ProfileImg src={gravatar.url(data.email,{s:'28px',d:'retro'})} alt={data.nickname}></ProfileImg>
+                        <ProfileImg src={gravatar.url(userData.email, {s: '28px', d: 'retro'})}
+                                    alt={userData.nickname}></ProfileImg>
                         {showUserMenu && (
-                            <Menu style={{right:0,top:30}} show={showUserMenu} onCloseModal={onClickUserProfile}>
+                            <Menu style={{right: 0, top: 30}} show={showUserMenu} onCloseModal={onClickUserProfile}>
                                 <ProfileModal>
-                                    <img src={gravatar.url(data.email,{s:'36px',d:'retro'})} alt=""/>
+                                    <img src={gravatar.url(userData.email, {s: '36px', d: 'retro'})} alt=""/>
                                     <div>
-                                        <span id={"profile-name"}>{data.nickname}</span>
+                                        <span id={"profile-name"}>{userData.nickname}</span>
                                         <span id={"profile-active"}>Active</span>
                                     </div>
                                 </ProfileModal>
@@ -67,7 +73,16 @@ const Workspace: FC = ({children}) => {
             </Header>
             <button onClick={onLogout}>로그아웃</button>
             <WorkspaceWrapper>
-                <Workspaces>test</Workspaces>
+                <Workspaces>
+                    {userData.Workspaces.map((ws) => {
+                        return (
+                            <Link key={ws.id} to={`/workspace/${123}/channel/일반`}>
+                                <WorkspaceButton>{ws.name.slice(0, 1).toUpperCase()}</WorkspaceButton>
+                            </Link>
+                        );
+                    })}
+                    <AddButton onClick={onClickCreate}>+</AddButton>
+                </Workspaces>
                 <Channels>
                     <WorkspaceName>Sleact</WorkspaceName>
                     <MenuScroll>menu scroll</MenuScroll>
