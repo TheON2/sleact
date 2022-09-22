3a5c65ee870f3cec6855270f073509538907700a
TheON2 <sniperad@naver.com>
2022-09-22 20:28:09 +0900

    Update : App.index apply Dynamic Routing

--- alecture/layouts/Workspace/index.tsx
+++ alecture/layouts/Workspace/index.tsx
@@ -2,7 +2,7 @@ import React, {VFC, useCallback, useState} from 'react';
 import useSWR from "swr";
 import fetcher from "@utils/fetcher";
 import axios from 'axios';
-import {Redirect, Route, Switch} from "react-router";
+import {Redirect, Route, Switch, useParams} from "react-router";
 import {
     AddButton,
     Channels, Chats,
@@ -17,7 +17,7 @@ import gravatar from 'gravatar';
 import loadable from "@loadable/component";
 import Menu from "@components/Menu";
 import {Link} from 'react-router-dom';
-import {IUser} from "@typings/db";
+import {IChannel, IUser} from "@typings/db";
 import {Button, Input, Label } from '@pages/SignUp/styles';
 import useInput from "@hooks/useInput";
 import Modal from '@components/Modal';
@@ -35,9 +35,11 @@ const Workspace: VFC = () => {
     const [newWorkspace,onChangeNewWorkspace , setNewWorkspace] = useInput(' ');
     const [newUrl,onChangeNewUrl , setNewUrl] = useInput(' ');
 
+    const { workspace } = useParams<{workspace : string}>();
     const { data: userData, error, revalidate, mutate} = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher, {
         dedupingInterval: 2000, // 2초
     },);
+    const {data:channelData} = useSWR<IChannel[]>(userData ? `http://localhost:3095/api/workspaces/${workspace}/channels` : null ,fetcher);
 
     const onLogout = useCallback(() => {
         axios.post('http://localhost:3095/api/users/logout', null, {
@@ -147,12 +149,15 @@ const Workspace: VFC = () => {
                                 <button onClick={onLogout}>로그아웃</button>
                             </WorkspaceModal>
                         </Menu>
+                        {channelData?.map((v)=>(
+                            <div>{ v.name}</div>
+                        ))}
                     </MenuScroll>
                 </Channels>
                 <Chats>
                     <Switch>
-                        <Route path="/workspace/channel" component={Channel}/>
-                        <Route path="/workspace/dm" component={DirectMessage}/>
+                        <Route path="/workspace/:workspace/channel/:channel" component={Channel}/>
+                        <Route path="/workspace/:workspace/dm/:id" component={DirectMessage}/>
                     </Switch>
                 </Chats>
             </WorkspaceWrapper>
@@ -169,7 +174,7 @@ const Workspace: VFC = () => {
                     <Button type="submit">생성하기</Button>
                 </form>
             </Modal>
-            <CreateChannelModal show={showCreateChannelModal} onCloseModal={onCloseModal}/>
+            <CreateChannelModal show={showCreateChannelModal} onCloseModal={onCloseModal} setShowCreateChannelModal={setShowCreateChannelModal}/>
         </div>
     );
 };
--- alecture/pages/SignUp/index.tsx
+++ alecture/pages/SignUp/index.tsx
@@ -63,7 +63,7 @@ const SingUp = () => {
     }
 
     if(data){
-        return <Redirect to="/workspace/channel"/>;
+        return <Redirect to="/workspace/sleact/channel/일반" />;
     }
 
     return (
