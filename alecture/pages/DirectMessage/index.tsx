0d84525dc3fccf11b36008a09e969a1126b26c3e
TheON2 <sniperad@naver.com>
2022-09-23 12:47:22 +0900

    Create : DirectMessage Page

--- alecture/pages/DirectMessage/index.tsx
+++ alecture/pages/DirectMessage/index.tsx
@@ -1,9 +1,33 @@
 import Workspace from "@layouts/Workspace";
 import React from "react";
 
+import gravatar from "gravatar";
+import {Container, Header } from "./styles";
+import useSWR from "swr";
+import {IUser} from "@typings/db";
+import fetcher from "@utils/fetcher";
+import {useParams} from "react-router";
+
 const DirectMessage = () => {
+    const { workspace , id } = useParams<{workspace: string ,id: string}>();
+    const { data: userData, error, revalidate, mutate} = useSWR(`/api/workspaces/${workspace}/members/${id}`, fetcher, {
+        dedupingInterval: 2000, // 2초
+    },);
+    const { data : myData } = useSWR('/api/users',fetcher);
+
+    if( !userData || !myData){
+        return null;
+    }
+
     return(
-            <div>DM</div>
+        <Container>
+            <Header>
+                <img src={gravatar.url(userData.email, { s: '24px',})} alt={userData.nickname}/>
+                <span>{userData.nickname}</span>
+            </Header>
+            <ChatList/>
+            <ChatBox/>
+        </Container>
     )
 }
 
