6fc2c9a2e027ab1796023d27de577fb19404653b
TheON2 <sniperad@naver.com>
2022-09-29 15:33:25 +0900

    Update : DirectMessage apply OptimisticUI

--- alecture/pages/DirectMessage/index.tsx
+++ alecture/pages/DirectMessage/index.tsx
@@ -1,5 +1,5 @@
 import Workspace from "@layouts/Workspace";
-import React, {useCallback, useRef} from "react";
+import React, {useCallback, useEffect, useRef} from "react";
 
 import gravatar from "gravatar";
 import {Container, Header} from "./styles";
@@ -32,15 +32,38 @@ const DirectMessage = () => {
     const scrollbarRef = useRef<Scrollbars>(null);
     const onSubmitForm = useCallback((e) => {
         e.preventDefault();
-        if (chat?.trim()) {
+        if (chat?.trim() && chatData) {
+            const savedChat = chat;
+            mutateChat((prevChatData)=>{
+                prevChatData?.[0].unshift({
+                    id: (chatData[0][0]?.id || 0) + 1,
+                    SenderId: myData.id,
+                    Sender: myData,
+                    ReceiverId: userData.id,
+                    Receiver: userData,
+                    content: savedChat,
+                    createdAt: new Date(),
+                });
+                return prevChatData;
+            },false)
+            .then(()=>{
+                setChat('');
+                scrollbarRef.current?.scrollToBottom();
+            });
             axios.post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
                 content: chat,
             }).then(() => {
                 revalidate();
-                setChat('');
             }).catch(console.error);
         }
-    }, [chat]);
+    }, [chat,chatData, myData, userData, workspace , id]);
+
+    // 로딩 시 스크롤바 제일 아래로
+    useEffect(()=>{
+        if(chatData?.length === 1){
+            scrollbarRef.current?.scrollToBottom();
+        }
+    },[chatData]);
 
     if (!userData || !myData) {
         return null;
