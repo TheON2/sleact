8286ae7444314034d96d122f84567474801ab6d6
TheON2 <sniperad@naver.com>
2022-09-29 15:42:33 +0900

    Fix : InfiniteScroll

--- alecture/components/ChatList/index.tsx
+++ alecture/components/ChatList/index.tsx
@@ -1,39 +1,40 @@
-import React, {FC, useCallback, useRef, VFC , forwardRef} from "react";
+import React, {FC, useCallback, useRef, VFC, forwardRef, RefObject} from "react";
 import {ChatZone, Section, StickyHeader} from "./styles";
 import {IDM} from "@typings/db";
 import Chat from "@components/Chat";
 import {Scrollbars} from 'react-custom-scrollbars';
 
 interface Props {
-    chatSections: {[key:string]: IDM[]};
-    setSize: (f:(size:number) => number) => Promise<IDM[][] | undefined>;
-    isEmpty: boolean;
+    chatSections: { [key: string]: IDM[] };
+    setSize: (f: (size: number) => number) => Promise<IDM[][] | undefined>;
     isReachingEnd: boolean;
+    scrollRef: RefObject<Scrollbars>;
 }
 
-const ChatList = forwardRef<Scrollbars, Props>(({chatSections,setSize,isEmpty,isReachingEnd} , ref) => {
-    const onScroll = useCallback((values)=>{
-        if( values.scrollTop === 0 && !isReachingEnd) {
+const ChatList: VFC<Props> = (({chatSections, setSize, isReachingEnd,scrollRef}) => {
+    const onScroll = useCallback((values) => {
+        if (values.scrollTop === 0 && !isReachingEnd) {
             console.log('가장 위');
             // 데이터 추가 로딩
             setSize((prevSize) => prevSize + 1)
                 .then(() => {
                     //스크롤 위치 유지
+                    scrollRef.current?.scrollTop(scrollRef.current?.getScrollHeight() - values.scrollHeight)
                 });
         }
-    },[]);
+    }, []);
 
     return (
         <ChatZone>
-            <Scrollbars autoHide ref={ref} onScrollFrame={onScroll}>
-                {Object.entries(chatSections).map(([date,chats])=>{
-                    return(
+            <Scrollbars autoHide ref={scrollRef} onScrollFrame={onScroll}>
+                {Object.entries(chatSections).map(([date, chats]) => {
+                    return (
                         <Section className={`section-${date}`} key={date}>
                             <StickyHeader>
                                 <button>{date}</button>
                             </StickyHeader>
-                            {chats.map((chat)=>(
-                                <Chat key={chat.id} data={chat} />
+                            {chats.map((chat) => (
+                                <Chat key={chat.id} data={chat}/>
                             ))}
                         </Section>
                     )
