070e76c1b2dc1fffdbe413c025ab27a5968a76c4
TheON2 <sniperad@naver.com>
2022-10-28 17:02:05 +0900

    Updata : add ValidationPipe

--- a-nest/src/users/users.service.ts
+++ a-nest/src/users/users.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Users } from '../output/entities/Users';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -14,21 +19,9 @@ export class UsersService {
   getUser() {}
 
   async postUsers(email: string, nickname: string, password: string) {
-    if (!email) {
-      throw new HttpException('이메일이 없어요', 400);
-    }
-
-    if (!nickname) {
-      throw new HttpException('닉네임이 없어요', 400);
-    }
-
-    if (!password) {
-      throw new HttpException('비번이 없어요', 400);
-    }
-
     const user = await this.usersRepository.findOne({ where: { email } });
     if (user) {
-      throw new Una('이미 존재하는 사용자입니다.');
+      throw new UnauthorizedException('이미 존재하는 사용자입니다.');
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
